d941cf18dbf0cbdb5e76aa1b0db8e981fd2de1c6
Cristian Silva <silva.cristian.43@hotmail.com>
2022-09-22 17:22:43 -0300

    Actualice boton enter

--- Frontend/Inicio.Designer.cs
+++ Frontend/Inicio.Designer.cs
@@ -35,12 +35,14 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(72)))), ((int)(((byte)(146)))));
+            this.button1.ForeColor = System.Drawing.Color.Black;
             this.button1.Location = new System.Drawing.Point(250, 316);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(64, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "Entrar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
